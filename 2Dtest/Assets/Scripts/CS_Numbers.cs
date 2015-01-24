c1d0ac0beada5d7119c40ff8cae58728b75ad33a
dxman <david.crissman@gmail.com>
2015-01-24 13:20:56 -0500

    -Update

--- 2Dtest/Assets/Scripts/CS_Numbers.cs
+++ 2Dtest/Assets/Scripts/CS_Numbers.cs
@@ -4,57 +4,24 @@ using System.Collections;
 public class CS_Numbers : MonoBehaviour
 {
 	public CS_RandomNumber Numbers;
-	public CS_PlayerHealth heatlth;
+	public CS_PlayerBehavior player;
 
 	public bool isTouched;
 
-	public int saved = 0;
 	public int NumberIndex;
 	public int Number;
-	public int done = 0;
 
 
 	// Use this for initialization
 	void Start () 
 	{
 		Numbers = GameObject.Find ("GameState").GetComponent <CS_RandomNumber> ();
-		heatlth = GameObject.Find ("Player's Health").GetComponent<CS_PlayerHealth> ();
 		Number = Numbers.numbers[NumberIndex];
 	}
 
 	void Update ()
 	{
-		if (isTouched == true && done == 0)
-		{
-			if (Input.GetButton("Fire1"))
-			{
-				if (Number <= 5)
-				{
-					heatlth.curHealth -= 20;
-					print ("Oww!");
-					renderer.material.color = Color.gray;
-					isTouched = false;
-					done = 1;
-				}
-
-				else if (Number > 5 && (Number <= 8))
-				{
-					saved += 1;
-					print ("You Saved Me!");
-					renderer.material.color = Color.gray;
-					isTouched = false;
-					done = 1;
-				}
-
-				else 
-				{
-					print ("It's Fucking Nothing");
-					renderer.material.color = Color.gray;
-					isTouched = false;
-					done = 1;
-				}
-			}
-		}
+		
 	}
 	
 	void OnTriggerEnter2D(Collider2D other)
@@ -69,4 +36,29 @@ public class CS_Numbers : MonoBehaviour
 	{
 		isTouched = false;
 	}
+
+    void PlayerInteract()
+    {
+        if (isTouched == true)
+        {
+            if (Number <= 5)
+            {
+                player.curHealth -= 20;
+                print("Oww!");
+                isTouched = false;
+            }
+
+            else if (Number > 5 && (Number <= 8))
+            {
+                print("You Saved Me!");
+                isTouched = false;
+            }
+
+            else
+            {
+                print("It's Fucking Nothing");
+                isTouched = false;
+            }
+        }
+    }
 }
