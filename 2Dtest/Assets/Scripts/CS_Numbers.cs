17c0a13f5324682d222ee45656e684c188ddfb6b
Nathon Kepley <nathonkepley@stu.aii.edu>
2015-01-24 11:23:09 -0500

    test 2.0.2

--- 2Dtest/Assets/Scripts/CS_Numbers.cs
+++ 2Dtest/Assets/Scripts/CS_Numbers.cs
@@ -8,8 +8,10 @@ public class CS_Numbers : MonoBehaviour
 
 	public bool isTouched;
 
+	public int saved = 0;
 	public int NumberIndex;
 	public int Number;
+	public int done = 0;
 
 
 	// Use this for initialization
@@ -22,7 +24,7 @@ public class CS_Numbers : MonoBehaviour
 
 	void Update ()
 	{
-		if (isTouched == true)
+		if (isTouched == true && done == 0)
 		{
 			if (Input.GetButton("Fire1"))
 			{
@@ -30,19 +32,26 @@ public class CS_Numbers : MonoBehaviour
 				{
 					heatlth.curHealth -= 20;
 					print ("Oww!");
+					renderer.material.color = Color.gray;
 					isTouched = false;
+					done = 1;
 				}
 
 				else if (Number > 5 && (Number <= 8))
 				{
+					saved += 1;
 					print ("You Saved Me!");
+					renderer.material.color = Color.gray;
 					isTouched = false;
+					done = 1;
 				}
 
 				else 
 				{
 					print ("It's Fucking Nothing");
+					renderer.material.color = Color.gray;
 					isTouched = false;
+					done = 1;
 				}
 			}
 		}
