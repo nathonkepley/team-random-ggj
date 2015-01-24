9aaa4f4d6252237a584dd2ae6758fea50f969033
Nathon Kepley <nathonkepley@stu.aii.edu>
2015-01-23 20:54:20 -0500

    2D Test

---
+++ 2Dtest/Assets/Scripts/CS_Movement.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CS_Movement : MonoBehaviour 
+{
+	public float Speed = 2f;
+	public float jumpHeight = 100f;
+	public float runSpeed =    5f;
+
+	public bool Grounded;
+
+
+
+	// Use this for initialization
+	void Start () 
+		{
+		
+		}
+	
+	// Update is called once per frame
+	void Update () 
+		{
+			if (Input.GetKey (KeyCode.D))
+				{
+					transform.Translate (Vector2.right * Speed * Time.deltaTime);
+					transform.eulerAngles = new Vector3 (0,0,0);
+				}
+			
+			if (Input.GetKey (KeyCode.A))
+				{
+					transform.Translate (Vector2.right * Speed * Time.deltaTime);
+					transform.eulerAngles = new Vector3 (0,180,0);
+				}
+
+			if (Input.GetKeyDown (KeyCode.LeftShift))
+				{
+					Speed = runSpeed;
+					jumpHeight = 150;
+				}
+			if (Input.GetKeyUp (KeyCode.LeftShift))
+				{
+					Speed = 2f;
+					jumpHeight = 100;
+				}
+			if (Grounded == true)
+				{
+					if (Input.GetKey (KeyCode.Space))
+						{
+							gameObject.rigidbody.AddForce (Vector3.up * jumpHeight);
+						}
+				}
+		}
+
+	void OnCollisionEnter(Collision other)
+		{
+			if (other.gameObject.tag == "Ground")
+				{
+					Grounded = true;
+				}
+		}
+
+	void OnCollisionExit (Collision other)
+		{
+			Grounded = false;
+		}
+}
---
+++ 2Dtest/Assets/Scripts/CS_PlayerHealth.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CS_PlayerHealth : MonoBehaviour 
+{
+	public int curHealth = 100;
+	public int maxHealth = 100;
+
+	public GameObject plyHealth;
+	public CS_Checkpoints checkpoint;
+
+	// Use this for initialization
+	void Start () 
+	{
+		plyHealth = GameObject.Find ("Player's Health"); 
+
+		if (curHealth <= 0)
+		{
+			curHealth = 0;
+		}
+
+		if (curHealth >= 100)
+		{
+			curHealth = 100;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		guiText.text = curHealth + "/" + maxHealth;
+	}
+}
