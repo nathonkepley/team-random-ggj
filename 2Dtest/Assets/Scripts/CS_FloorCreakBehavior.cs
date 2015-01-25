c6398a0613496d82f29c471b1b27792ab7a2dc6a
dxman <david.crissman@gmail.com>
2015-01-24 21:05:32 -0500

    -Experiments with lighting -Began replacing placeholder sprites with final ones

---
+++ 2Dtest/Assets/Scripts/CS_FloorCreakBehavior.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CS_FloorCreakBehavior : MonoBehaviour 
+{
+    public CS_GameState state;
+
+	public int damageDone;
+
+	public bool isTouched;
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (isTouched == true)
+		{
+            state.curHealth = state.curHealth - damageDone;
+
+			if (gameObject.tag == "Enemy")
+			{
+				gameObject.SetActive (false);
+			}
+		}
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			isTouched = true;
+		}
+	}
+
+	void OnTriggerExit ()
+	{
+		isTouched = false;
+	}
+}
